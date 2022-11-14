c36d5542f5d76d50d43ad6b8e10d5cfe0303b0e1
J.B.C <juanbicortelezzi@gmail.com>
2022-11-13 21:37:31 -0300

    all site working as expected

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), solidJs(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
--- src/utils/lib.ts
+++ src/utils/lib.ts
@@ -1,3 +1,21 @@
+import { z } from "zod";
+
+export const markdownFrontmatterValidator = z
+  .object({
+    title: z.string(),
+    description: z.string(),
+    image: z.string(),
+    date: z.string(),
+  })
+  .transform((frontmatter) => {
+    return { ...frontmatter, date: new Date() };
+  });
+
+export type MarkdownFrontmatter = z.input<typeof markdownFrontmatterValidator>;
+export type MarkdownFrontmatterOut = z.output<
+  typeof markdownFrontmatterValidator
+>;
+
 // tailwind className string builder
 export const classNames = (...classes: (string | boolean)[]) =>
   classes.filter(Boolean).join(" ");

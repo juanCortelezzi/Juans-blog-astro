3b994b1f79355b623825729c1f0d212ff424d693
J.B.C <juanbicortelezzi@gmail.com>
2022-11-16 20:15:29 -0300

    replace solid component with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), solidJs(), image({
+  integrations: [mdx(), tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
   })]
 });
